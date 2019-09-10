cea23f8677909b63c4d3380038d62825d7e27833
nikkiricks <nikki.ricks@gmail.com>
2019-09-10 15:25:15 +1000

    created game logic

--- memory_game/main.js
+++ memory_game/main.js
@@ -23,13 +23,13 @@ var cards = [
 
 var cardsInPlay = [];
 
-var checkForMatch = function () {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
-} else {
-  console.log("Sorry, try again.");
+var checkForMatch = function() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
 }
-};
 
 
 var flipCard = function() {
