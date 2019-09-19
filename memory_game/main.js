485bfe7a948a90b8d22c42f85a3e6c5c79f4faf3
nikkiricks <nikki.ricks@gmail.com>
2019-09-19 20:26:41 +1000

    created game logic

--- memory_game/main.js
+++ memory_game/main.js
@@ -1,45 +1,8 @@
-var cards = [
-{
-	rank: "queen",
-	suit: "hearts",
-	cardImage: "images/queen-of-hearts.png"
-},
-{
-	rank: "queen",
-	suit: "diamonds",
-	cardImage: "images/queen-of-diamonds.png"
-},
-{
-	rank: "king",
-	suit: "hearts",
-	cardImage: "images/king-of-hearts.png"
-},
-{
-	rank: "king",
-	suit: "diamonds",
-	cardImage: "images/king-of-diamonds.png"
-}
-];
-
-var cardsInPlay = [];
-
-var checkForMatch = function() {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
-  } else {
-    alert("Sorry, try again.");
-  }
-}
-
-
-var flipCard = function() {
-	var cardId = parseInt(this.getAttribute('data-id'),10);
-	console.log("User flipped " + (cards[cardId].rank));
-	this.src = cards[cardId].cardImage;
-	cardsInPlay.push(cards[cardId].rank);
-	checkForMatch();
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
-};
-
+console.log("Up and running!");
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardFour);
 
